ed585a58facb372e0d71b4e89beab8253835fdc2
Boyoung KIM <boyoung@vanary.com>
2019-01-29 16:59:02 +0900

    Add files via upload

---
+++ precache-manifest.f28a3587450f9d710276ab727b76ccd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "47eae9cd0d75248565ee",
+    "url": "/javascript-web-todo/static/js/runtime~main.47eae9cd.js"
+  },
+  {
+    "revision": "771a3290c104e8922e57",
+    "url": "/javascript-web-todo/static/js/main.771a3290.chunk.js"
+  },
+  {
+    "revision": "fbb3b5d735f534a1e08d",
+    "url": "/javascript-web-todo/static/js/1.fbb3b5d7.chunk.js"
+  },
+  {
+    "revision": "771a3290c104e8922e57",
+    "url": "/javascript-web-todo/static/css/main.ef48cd35.chunk.css"
+  },
+  {
+    "revision": "9fac94ed65cfb3641dc26ddc040b31b5",
+    "url": "/javascript-web-todo/index.html"
+  }
+];
